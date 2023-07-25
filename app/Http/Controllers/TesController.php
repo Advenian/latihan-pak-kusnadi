60b648214c9c649dc951205b1d6634b062b716d2
pradipta <mpradipta.arya02@gmail.com>
2023-07-25 11:12:56 +0700

    Added outside assets and put it in welcome.blade

--- app/Http/Controllers/TesController.php
+++ app/Http/Controllers/TesController.php
@@ -10,18 +10,20 @@ class TesController extends Controller
     public function tampilkanSession(Request $request){
         if($request->session()->has('nama')){
             echo $request->session()->get('nama');
+            echo $request->session()->get('kelas');
+            echo $request->session()->get('absen');
         }else{
             echo 'Tidak ada data dalam session.';
         }
     }
 
     public function buatSession(Request $request){
-        $request->session()->put('nama','Dipta');
+        $request->session()->put(['nama' => 'Dipta', 'kelas' => 'xii-rpl', 'absen' => '20']);
         echo "Data telah ditambahkan ke session";
     }
 
     public function hapusSession(Request $request){
-        $request->session()->forget('nama');
+        $request->session()->forget(['nama', 'kelas', 'absen']);
         echo "Data telah dihapus dari session";
     }
 }
